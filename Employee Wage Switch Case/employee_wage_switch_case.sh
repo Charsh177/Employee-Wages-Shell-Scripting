1279312e0f0875f5bc5a2204f1d745861acc3125
Charsh177 <harshchougule177@gmail.com>
2022-05-02 01:21:04 +0530

    switch case

---
+++ Employee Wage Switch Case/employee_wage_switch_case.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+# Switch Case Wages
+
+isPartTime=1
+isFullTime=2
+empRatePerHr=20
+empCheck=$((RANDOM%3))
+
+case $empCheck in
+	$isFullTime)
+                empHrs=8 ;;
+	$isPartTime)
+		empHrs=4 ;;
+	*)
+		empHrs=0 ;;
+esac
+
+salary=$(($empHrs*$empRatePerHr))
+
+echo "Salary Of Employee "$salary
